701a13fa62c0d1f523317535d634842feb1950ba
NikitDudinov <dudinovnik@yandex.ru>
2024-10-30 20:52:10 +0300

    Поправил App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -10,14 +10,14 @@ type AppProps = {
 }
 function App({presentation}: AppProps) {
   const curSlide = presentation.slides.length > 0 
-  ? (presentation.slides.find(slide => slide.id == presentation.select.selectedSlideId) || presentation.slides[0]) 
+  ? (presentation.slides.find(slide => slide.id == presentation.selection.selectedSlideId) || presentation.slides[0]) 
   : null;
   return (
       <>
           <TopPanel title={presentation.title} background={curSlide?.background}></TopPanel>
           <div className={styles.container}>
-              <SlidesList slides={presentation.slides} selectSlideId={presentation.select.selectedSlideId}></SlidesList>
-              <Workspace slide={curSlide} selectElemnets={presentation.select.elementsId}></Workspace>
+              <SlidesList slides={presentation.slides} selectSlideId={presentation.selection.selectedSlideId}></SlidesList>
+              <Workspace slide={curSlide} selectElemnets={presentation.selection.elementsId}></Workspace>
           </div>
       </>
   )
